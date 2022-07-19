77943149f260f4693e0a7ed6387f28ec06b7cd3f
arifpehlivan <arifpehlivanapap@gmail.com>
2022-07-20 02:29:38 +0300

    add header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <ul>
+                <li><a href="/#" >Home</a></li>
+                <li><a href="/#" >About</a></li>
+                <li><a href="/#" >Timeline</a></li>
+                <li><a href="/#" ><h2> Medic Cares </h2><strong className='clearfix'> Healt Specialist </strong></a></li>
+                <li><a href="/#" >Tesitimonials</a></li>
+                <li><a href="/#" >Booking</a></li>
+                <li><a href="/#" >Contact</a></li>
+            </ul>         
+        </div>
+    )
+}
+
+export default Header
